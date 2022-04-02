ef0f764ed60ca6ad4f9cc8fe3c81c4cfd5943fc1
Arthur Alcantara <vinnycamara1@gmail.com>
2022-04-02 18:26:27 -0300

    Ultimo commit

--- scripts.js
+++ scripts.js
@@ -53,11 +53,38 @@ function selectDissert(food) {
 }
 
 function checkList() {
+  const button = document.querySelector(".button");
+  const h3 = button.firstElementChild;
+
+  if (checkButtons()) {
+    button.style.backgroundColor = "#32B72F";
+    console.log(button);
+    h3.innerHTML = "Fechar pedido";
+    h3.style.fontWeight = "700";
+  } else {
+    button.style.backgroundColor = "#CBCBCB";
+    h3.innerHTML = "Selecione os 3 itens <br> para fechar o pedido";
+    h3.style.fontWeight = "400";
+  }
+}
+
+function checkButtons() {
   const botao = document.querySelector(".dish.checked");
   const botao2 = document.querySelector(".drink.checked");
   const botao3 = document.querySelector(".dissert.checked");
-
   if (botao !== null && botao2 !== null && botao3 !== null) {
-    console.log("habilitar");
+    return true;
+  }
+  return false;
+}
+
+function zapButton() {
+  if (checkButtons()) {
+    const mensagem = `Olá, gostaria de fazer o pedido:\n- Prato: Frango Yin Yang\n- Bebida: Coquinha Gelada\n- Sobremesa: Pudim\nTotal: R$ 27.70`;
+    const URL = "https://wa.me/5581982411222?text=";
+    const encode = encodeURIComponent(mensagem);
+    console.log(encode);
+
+    window.open(URL + encode, "_blank");
   }
 }
